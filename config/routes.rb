a97a4797b5e1a03157b9f58be3d99c12f29fc30c
linguyen <nguyenvanlin01041994@gmail.com>
2016-11-18 12:33:50 +0700

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   resources :users
   get 'users/new'
 
   get 'users/index'
 
   get 'users/show'
+
+  get '/sign_up', to: 'users#new'
+
+  get '/login',to: 'sessions#new'
+
+  post '/login',to: 'sessions#create'
+
+  delete '/logout',to: 'sessions#destroy'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
