d34388be1b89fc62fd11d7d016a2f3612b6beaa5
linguyen <nguyenvanlin01041994@gmail.com>
2016-11-18 11:52:27 +0700

    heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,4 +37,9 @@ class UsersController < ApplicationController
     	end
     end
 
+    def user_params
+	  	params.require(:user).permit(:name,:email,:password,
+	  		:password_confirmation)
+	end
+
 end
