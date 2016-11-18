f281a607f73aa9a5d440f15c63be196dffeed337
linguyen <nguyenvanlin01041994@gmail.com>
2016-11-18 14:20:56 +0700

    heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 		if @user.save
 			log_in @user
 			flash[:success] ="Welcome to the Sample App!"
-			redirect_to @user
+			redirect_to bookrooms_path
 		else
 			render 'new'
 		end
