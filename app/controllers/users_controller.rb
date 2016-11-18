e0196d936c5f59b84150f67a97c7177f64cb2010
linguyen <nguyenvanlin01041994@gmail.com>
2016-11-18 11:00:04 +0700

    heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,40 @@
 class UsersController < ApplicationController
-  def new
-  end
-
   def index
-  end
+    	@users = User.paginate(page: params[:page])
+    end
 
-  def show
-  end
+	def show
+		@user=User.find(params[:id])
+		@entries = @user.entries.paginate(page: params[:page])
+	end
+
+	def new
+		@user=User.new
+	end
+
+	def create
+		@user=User.new(user_params)
+		if @user.save
+			log_in @user
+			flash[:success] ="Welcome to the Sample App!"
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@user=User.find(params[:id])
+	end
+
+    def update
+    	@user=User.find(params[:id])
+    	if @user.update_attributes(user_params)
+    		flash[:success]="Profile updated"
+    		redirect_to @user
+    	else
+    		render 'edit'
+    	end
+    end
+
 end
