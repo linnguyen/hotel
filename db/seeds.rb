ebca7ce2dead94eafab7e5f241567d32b1197cd3
linguyen <nguyenvanlin01041994@gmail.com>
2016-11-19 18:40:51 +0700

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Permission.create!(role: 1,name: "admin")
+Permission.create!(role: 2,name: "mod")
+Permission.create!(role: 3,name: "user")
